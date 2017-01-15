6dcc21b99d6a5e88bd46d8409e65223dc3030c4c
Aleksandr Fedotov <fedotov@jivosite.com>
2017-01-15 15:22:57 +0300

    Шаг пятый - проксинг пакетов

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
-	"io"
+	dbms "github.com/orderbynull/myproxy/mysql"
 )
 
 const MYSQL = "127.0.0.1:3306"
@@ -12,15 +13,21 @@ const PROXY = "127.0.0.1:3305"
 const COM_QUERY = 3
 const COM_STMT_PREPARE = 22
 
-func appToMysql (app net.Conn, mysql net.Conn){
-	io.Copy(mysql, app)
+func appToMysql(app net.Conn, mysql net.Conn) {
+	for{
+		_, err := dbms.ProxyPacket(app, mysql)
+		if err != nil{
+			break
+		}
+	}
+
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	mysql, err := net.Dial("tcp", MYSQL)
-	if err != nil{
+	if err != nil {
 		log.Fatalf("%s: %s", "ERROR", err.Error())
 		return
 	}
--- mysql/packet.go
+++ mysql/packet.go
@@ -1,1 +1,56 @@
 package mysql
+
+import (
+	"io"
+	"net"
+	"errors"
+)
+
+//Заранее определим возможные ошибки
+var ErrWritePacket = errors.New("error while writing packet payload")
+
+func ReadPacket(conn net.Conn) ([]byte, error) {
+	header := []byte{0, 0, 0, 0}
+
+	if _, err := io.ReadFull(conn, header); err == io.EOF {
+		return nil, io.ErrUnexpectedEOF
+	} else if err != nil {
+		return nil, err
+	}
+
+	bodyLength := int(uint32(header[0]) | uint32(header[1])<<8 | uint32(header[2])<<16)
+
+	body := make([]byte, bodyLength)
+
+	n, err := io.ReadFull(conn, body)
+	if err == io.EOF {
+		return nil, io.ErrUnexpectedEOF
+	} else if err != nil {
+		return nil, err
+	}
+
+	return append(header, body[0:n]...), nil
+}
+
+func WritePacket(pkt []byte, conn net.Conn) (int, error) {
+	n, err := conn.Write(pkt)
+	if err != nil {
+		return 0, ErrWritePacket
+	}
+
+	return n, nil
+}
+
+func ProxyPacket(src, dst net.Conn) ([]byte, error) {
+	pkt, err := ReadPacket(src)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = WritePacket(pkt, dst)
+	if err != nil {
+		return nil, err
+	}
+
+	return pkt, nil
+}
